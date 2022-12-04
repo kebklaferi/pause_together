f33f6988c8538b2ef6468a968dd3df6513dc18c1
larapodjavrsek <lara.podjavrsek@gmail.com>
2022-12-04 18:47:39 +0100

    vzpostavljen SpringBoot #PAUSE_TOGETHER-10 DONE Time 1h

---
+++ pause_together/src/main/java/com/ris/pause_together/PauseTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.ris.pause_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauseTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauseTogetherApplication.class, args);
+	}
+
+}
---
+++ pause_together/src/test/java/com/ris/pause_together/PauseTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ris.pause_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauseTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
