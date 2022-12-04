19781035e5bac4a9aeb53cf1be3afb8c452f1385
klarakeb <klara.kebric@student.um.si>
2022-12-04 22:45:25 +0100

    razvrstitev razredov #PAUSE_TOGETHER-11 DONE 2h

--- pause_together/src/main/java/com/ris/pause_together/models/Prosnja.java
+++ pause_together/src/main/java/com/ris/pause_together/models/Prosnja.java
@@ -3,9 +3,41 @@ package com.ris.pause_together.models;
 public class Prosnja {
 	private Skupina skupina;
 	public Uporabnik posiljatelj;
-	public VodjaSkupine prosnje;
+	public Uporabnik prejemnik;
+
+
+	public Prosnja(){}
+
+	public Prosnja(Skupina s, Uporabnik od, Uporabnik za){
+		this.skupina = s;
+		this.posiljatelj = od;
+		this.prejemnik = za;
+	}
+
+	public Skupina getSkupina() {
+		return skupina;
+	}
+
+	public void setSkupina(Skupina skupina) {
+		this.skupina = skupina;
+	}
+
+	public Uporabnik getPosiljatelj() {
+		return posiljatelj;
+	}
+
+	public void setPosiljatelj(Uporabnik posiljatelj) {
+		this.posiljatelj = posiljatelj;
+	}
+
+	public Uporabnik getPrejemnik() {
+		return prejemnik;
+	}
+
+	public void setPrejemnik(Uporabnik prejemnik) {
+		this.prejemnik = prejemnik;
+	}
 
 	public void potrdiProsnjo() {
-		throw new UnsupportedOperationException();
 	}
 }
--- pause_together/src/main/java/com/ris/pause_together/models/Seznam.java
+++ pause_together/src/main/java/com/ris/pause_together/models/Seznam.java
@@ -5,13 +5,41 @@ import java.util.ArrayList;
 public class Seznam {
 	private String naziv;
 	private ArrayList<Vsebina> vsebine;
-	public Uporabnik ima_sezname;
+	private Uporabnik ima_sezname;
 
 	public Seznam() {
-		throw new UnsupportedOperationException();
+		vsebine = new ArrayList<>();
+	}
+	public Seznam(String naz) {
+		this();
+		this.naziv = naz;
 	}
 
-	public Seznam(String naz) {
-		throw new UnsupportedOperationException();
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public ArrayList<Vsebina> getVsebine() {
+		return vsebine;
+	}
+
+	public void setVsebine(ArrayList<Vsebina> vsebine) {
+		this.vsebine = vsebine;
+	}
+
+	public Uporabnik getIma_sezname() {
+		return ima_sezname;
+	}
+
+	public void setIma_sezname(Uporabnik ima_sezname) {
+		this.ima_sezname = ima_sezname;
+	}
+
+	public void dodajVsebino(Vsebina vseb){
+		vsebine.add(vseb);
 	}
 }
