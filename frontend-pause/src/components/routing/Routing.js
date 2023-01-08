bd35c9345ecc0fded8f94ef6b09334cf16b6e644
klarakeb <klara.kebric@student.um.si>
2023-01-08 10:05:57 +0100

    frontend

--- frontend-pause/src/components/routing/Routing.js
+++
@@ -1,9 +0,0 @@
-import {Route, Routes} from "react-router-dom";
-
-export default function Routing(){
-    return(
-        <Routes>
-            <Route path="/" element={<p>Nekaj dela?</p>}/>
-        </Routes>
-    );
-}
